6585385c39b0475b12ab522aabfc48d9fb95f049
Roberto Duessmann <roberto.duessmann@gmail.com>
2019-05-21 21:49:57 +0200

    Revert "Fixed erros when parse description and wind attributes"

--- parser/parse.go
+++ parser/parse.go
@@ -12,7 +12,8 @@ func Parse(doc *goquery.Document, filters []string) (parsed string) {
 	for _, filter := range filters {
 		doc.Find(filter).Each(func(i int, s *goquery.Selection) {
 			if filter == "body > pre" {
-				parsed = retriveDescriptionData(doc, filter)
+				t := strings.TrimSpace(doc.Find(filter).Contents().Get(2).Data)
+				parsed = t
 			}
 			if _, err := strconv.Atoi(s.Text()); err == nil {
 				parsed = s.Text()
@@ -20,17 +21,4 @@ func Parse(doc *goquery.Document, filters []string) (parsed string) {
 		})
 	}
 	return
-}
-
-func retriveDescriptionData(doc *goquery.Document, filter string) string {
-	return retriveLastWord(retriveDescriptionFromHTML(doc, filter))
-}
-
-func retriveDescriptionFromHTML(doc *goquery.Document, filter string) string {
-	return strings.TrimSpace(doc.Find(filter).Contents().Get(0).Data)
-}
-
-func retriveLastWord(words string) string {
-	wordsInArray := strings.Split(words, " ")
-	return wordsInArray[len(wordsInArray)-1]
 }
