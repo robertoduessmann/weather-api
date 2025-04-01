a68c23c226c0c8f1d64105492fe9afb2331b9b55
Roberto Duessmann <roberto.duessmann@gmail.com>
2025-04-01 23:25:01 +0200

    Update manager.go

--- cache/manager.go
+++ cache/manager.go
@@ -7,36 +7,48 @@ import (
 
 type cacheManager struct {
 	avaliablesCache map[string]CacheClient
+	mu              sync.RWMutex
 }
 
-var instance CacheManager = nil
+var (
+	instance CacheManager
+	once     sync.Once
+)
 
 func NewCacheManager() CacheManager {
-	sync.OnceFunc(func() {
-		if instance == nil {
-			instance = cacheManager{
-				avaliablesCache: make(map[string]CacheClient),
-			}
+	once.Do(func() {
+		instance = &cacheManager{
+			avaliablesCache: make(map[string]CacheClient),
 		}
-	})()
+	})
 	return instance
 }
 
-func (m cacheManager) NewCache(name string, ttl time.Duration) CacheClient {
+func (m *cacheManager) NewCache(name string, ttl time.Duration) CacheClient {
+	m.mu.RLock()
 	client, found := m.avaliablesCache[name]
+	m.mu.RUnlock()
 	if found {
 		return client
 	}
 
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	cacheClient := NewCacheClient(ttl)
 	m.avaliablesCache[name] = cacheClient
 	return cacheClient
 }
 
-func (m cacheManager) Erase(name string) bool {
+func (m *cacheManager) Erase(name string) bool {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if _, exists := m.avaliablesCache[name]; exists {
+		delete(m.avaliablesCache, name)
+		return true
+	}
 	return false
 }
 
-func (m cacheManager) Delete(name string) bool {
-	return false
+func (m *cacheManager) Delete(name string) bool {
+	return m.Erase(name)
 }
